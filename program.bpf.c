758d93449eec3e232a8f3cb4d03c3ccfe77a6f2b
HitanshuPrasad <hitanshuprasad@gmail.com>
2024-08-01 18:07:59 +0530

    Added my new gadget

---
+++ program.bpf.c
@@ -0,0 +1,48 @@
+// program.bpf.c
+
+// Kernel types definitions
+// Check https://blog.aquasec.com/vmlinux.h-ebpf-programs for more details
+#include <vmlinux.h>
+
+// eBPF helpers signatures
+// Check https://man7.org/linux/man-pages/man7/bpf-helpers.7.html to learn
+// more about different available helpers
+#include <bpf/bpf_helpers.h>
+
+// Inspektor Gadget buffer
+#include <gadget/buffer.h>
+
+// Inspektor Gadget macros
+#include <gadget/macros.h>
+
+#define NAME_MAX 255
+
+struct event {
+	__u32 pid;
+	char comm[TASK_COMM_LEN];
+	char filename[NAME_MAX];
+};
+
+// events is the name of the buffer map and 1024 * 256 is its size.
+GADGET_TRACER_MAP(events, 1024 * 256);
+
+// [Optional] Define a tracer
+GADGET_TRACER(open, events, event);
+
+SEC("tracepoint/syscalls/sys_enter_openat")
+int enter_openat(struct syscall_trace_enter *ctx)
+{
+	struct event *event;
+
+	event = gadget_reserve_buf(&events, sizeof(*event));
+	if (!event)
+		return 0;
+
+	event->pid = bpf_get_current_pid_tgid() >> 32;
+
+	gadget_submit_buf(ctx, &events, event, sizeof(*event));
+	
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
